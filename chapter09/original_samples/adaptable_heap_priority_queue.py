18b436ac5ffca560a872bf6ff1c94b65db47def2
mikechen66 <875661606@qq.com>
2023-05-05 09:48:41 +0800

    Priority Queues

--- chapter09/original_samples/adaptable_heap_priority_queue.py
+++ chapter09/original_samples/adaptable_heap_priority_queue.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 from .heap_priority_queue import HeapPriorityQueue
 
 class AdaptableHeapPriorityQueue(HeapPriorityQueue):
