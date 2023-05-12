34256ec4e397ee4c9062cce86cf45c3f21fede9f
mikechen66 <875661606@qq.com>
2023-05-12 17:55:14 +0800

    Maps, Hash Tables and Skip Lists

---
+++ chapter10/sample_calls/chain_hash_map.py
@@ -0,0 +1,45 @@
+
+
+# chain_hash_map.py
+
+
+from hash_map_base import HashMapBase
+from unsorted_table_map import UnsortedTableMap
+
+
+"""
+
+The following three functions are realized in the subclass of ChainHapMap but 
+occur in the parent class HashMapBase
+
+_bucket_getitem()
+_bucket_setitem()
+_bucket_delitem()
+
+"""
+
+
+class ChainHashMap(HashMapBase):
+    """Hash map implemented with separate chaining for collision resolution."""
+    def _bucket_getitem(self, j, k):
+        bucket = self._table[j]
+        if bucket is None:
+            raise KeyError('Key Error: ' + repr(k))    # no match found
+        return bucket[k]                               # may raise KeyError
+    def _bucket_setitem(self, j, k, v):
+        if self._table[j] is None:
+            self._table[j] = UnsortedTableMap()        # bucket is new to the table
+        oldsize = len(self._table[j])
+        self._table[j][k] = v
+        if len(self._table[j]) > oldsize:              # key was new to the table
+            self._n += 1                               # increase overall map size
+    def _bucket_delitem(self, j, k):
+        bucket = self._table[j]
+        if bucket is None:
+            raise KeyError('Key Error: ' + repr(k))    # no match found
+        del bucket[k]                                  # may raise KeyError
+    def __iter__(self):
+        for bucket in self._table:
+            if bucket is not None:                     # a nonempty slot
+                for key in bucket:
+                    yield key
