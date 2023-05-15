cf5a3f0c3e37538e2a0a82d6491dbe267b8f9f3c
mikechen66 <875661606@qq.com>
2023-05-15 09:12:43 +0800

    Maps, Hash Tables and Skip Lists

--- chapter10/sample_calls/cost_performance.py
+++ chapter10/sample_calls/cost_performance.py
@@ -1,32 +1,53 @@
-
-
 # cost_performance.py
+
+"""
+
+1.All of the arguments are integers 
+
+2.Add the method of __len() to address the length
+
+"""
 
 
 from sorted_table_map import SortedTableMap
 
 
 class CostPerformanceDatabase:
-    """Maintain a database of maximal (cost,performance) pairs."""
+    # A database with maximal (cost, performance) pairs
     def __init__(self):
-        # Create an empty database.
-        self._M = SortedTableMap()             # or a more efficient sorted map
+        self._M = SortedTableMap()
+    def __len__(self):
+        return len(self._M)
     def best(self, c):
-        """
-        Return (cost,performance) pair with largest cost not exceeding c.
-        Return None if there is no such pair.
-        """
+        # return (c, p) pairs with largest cost not exceeding c.
         return self._M.find_le(c)
     def add(self, c, p):
-        # Add new entry with cost c and performance p.
-        # determine if (c,p) is dominated by an existing pair
-        other = self._M.find_le(c)              # other is at least as cheap as c
-        if other is not None and other[1] >= p: # if its performance is as good,
-            return                              # (c,p) is dominated, so ignore
-        self._M[c] = p                          # else, add (c,p) to database
-        # And now remove any pairs that are dominated by (c,p)
-        other = self._M.find_gt(c)              # other more expensive than c
+        other = self._M.find_le(c)
+        if other is not None and other[1] >= p:
+            return
+        self._M[c] = p
+        # remove any pairs that are dominated by (c, p)
+        other = self._M.find_gt(c)
         while other is not None and other[1] <= p:
             del self._M[other[0]]
             other = self._M.find_gt(c)
+
+
+if __name__ == '__main__':
+    cpd = CostPerformanceDatabase()
+    cpd.add(2000000, 200)
+    cpd.add(1600000, 160)
+    cpd.add(1400000, 140)
+    cpd.add(1200000, 120)
+    cpd.add(800000, 80)
+    print(cpd.best(1000000))
+    print(cpd.__len__())
+
+
+# Output:
+
+"""
+(800000, 80)
+5
+"""
 
