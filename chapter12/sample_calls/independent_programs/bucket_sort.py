4bcce365fe18b25dd01d6c5311eb21a5ce8d6383
mikechen66 <875661606@qq.com>
2023-05-21 12:10:18 +0800

    Sorting and Selection

---
+++ chapter12/sample_calls/independent_programs/bucket_sort.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env
+#encoding=utf-8
+
+
+# bucket_sort.py
+
+
+def bucketSort(array):
+    bucket = []
+    # Create empty buckets
+    for i in range(len(array)):
+        bucket.append([])
+    # Insert elements into their respective buckets
+    for j in array:
+        index_b = int(10 * j)
+        bucket[index_b].append(j)
+    # Sort the elements of each bucket
+    for i in range(len(array)):
+        bucket[i] = sorted(bucket[i])
+    # Get the sorted elements
+    k = 0
+    for i in range(len(array)):
+        for j in range(len(bucket[i])):
+            array[k] = bucket[i][j]
+            k += 1
+    return array
+
+
+if __name__ == '__main__':
+    array = [.42, .32, .33, .52, .37, .47, .51]
+    print("Sorted Array in descending order is")
+    print(bucketSort(array))
+
+# Output:
+
+"""
+Sorted Array in descending order is
+[0.32, 0.33, 0.37, 0.42, 0.47, 0.51, 0.52]
+"""
---
+++ chapter12/sample_calls/independent_programs/radix_sort.py
@@ -0,0 +1,55 @@
+
+
+# radix_sort.py
+
+
+# radix_sort use counting_sort function as a medium.
+
+
+# Using counting sort to sort the elements in the basis of significant places
+def counting_sort(array, place):
+    size = len(array)
+    output = [0] * size
+    count = [0] * 10
+    # Calculate count of elements
+    for i in range(0, size):
+        index = array[i] // place
+        count[index % 10] += 1
+    # Calculate cumulative count
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+    # Place the elements in sorted order
+    i = size - 1
+    while i >= 0:
+        index = array[i] // place
+        output[count[index % 10] - 1] = array[i]
+        count[index % 10] -= 1
+        i -= 1
+    for i in range(0, size):
+        array[i] = output[i]
+
+
+# Main function to implement radix sort
+def radix_sort(array):
+    # Get maximum element
+    max_element = max(array)
+    # Apply counting sort to sort elements based on place value.
+    place = 1
+    while max_element // place > 0:
+        counting_sort(array, place)
+        place *= 10
+
+
+if __name__ == "__main__":
+    data = [121, 432, 564, 23, 1, 45, 788]
+    print(data)
+    radix_sort(data)
+    print(data)
+
+
+# Output:
+
+"""
+[121, 432, 564, 23, 1, 45, 788]
+[1, 23, 45, 121, 432, 564, 788]
+"""
